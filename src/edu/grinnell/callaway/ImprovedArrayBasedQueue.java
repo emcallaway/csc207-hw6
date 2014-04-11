c76bc807aeeb3b4cf069c443b544e0adfa3dfe84
doughert1 <doughert1@grinnell.edu>
2014-04-11 15:50:36 -0500

    This is probably why Tony Stark created JARVIS
    Seriously, an AI would come in SO MUCH HELP right now

--- src/edu/grinnell/callaway/ImprovedArrayBasedQueue.java
+++ src/edu/grinnell/callaway/ImprovedArrayBasedQueue.java
@@ -1,9 +1,10 @@
 package edu.grinnell.callaway;
 
 public class ImprovedArrayBasedQueue<T>
-  extends ArrayBasedQueue<T>
+    extends
+      ArrayBasedQueue<T>
 {
-  
+
   int back;
 
   public ImprovedArrayBasedQueue(int capacity) throws Exception
@@ -11,52 +12,52 @@ public class ImprovedArrayBasedQueue<T>
     super(capacity);
     this.back = 0;
   } // ImprovedArrayBasedQueue(int capacity)
-  
+
   @Override
   public boolean isEmpty()
   {
     return this.size <= 0;
   } // isEmpty()
-  
+
   @Override
   public boolean isFull()
   {
-    return !this.isEmpty() && this.front == ((this.back)% this.values.length);
+    return !this.isEmpty() && this.front == ((this.back) % this.values.length);
   } // isFull()
-  
+
   @Override
   public void put(T val)
-      throws Exception
-    {
-      
-     if (this.isFull())
-        {
-          throw new Exception("no more room!");
-        } // this.isFull()
-     
-      this.values[this.back()] = val;
-      this.size++;
-    }
-  
+    throws Exception
+  {
+
+    if (this.isFull())
+      {
+        throw new Exception("no more room!");
+      } // this.isFull()
+
+    this.values[this.back()] = val;
+    this.size++;
+  } // put()
+
   @Override
   public T get()
-      throws Exception
+    throws Exception
+  {
+    if (this.isEmpty())
       {
-        if (this.isEmpty())
-          {
-            throw new Exception("empty");
-          } // if empty
-        // Grab and clear the element at the front of the queue
-        T result = this.values[this.front];
-        
-        this.values[this.front] = null;
-        this.front = (this.front+1)% (this.values.length);
-        // We're removing an element, so decrement the size
-        --this.size;
-        // And we're done
-        return result;
-        
-      }
+        throw new Exception("empty");
+      } // if empty
+    // Grab and clear the element at the front of the queue
+    T result = this.values[this.front];
+    this.values[this.front] = null;
+    this.front = (this.front + 1) % (this.values.length);
+    // We're removing an element, so decrement the size
+    --this.size;
+    // And we're done
+    return result;
+
+  } // get()
+
   @Override
   public T peek()
     throws Exception
@@ -67,16 +68,20 @@ public class ImprovedArrayBasedQueue<T>
       } // if empty
     return this.values[this.front];
   } // peek()
-  
+
   @Override
+  /**
+   * Figure out where the next element should go and advance to the
+   * next location.
+   */
   int back()
   {
-    if (this.back == this.values.length)
-      { 
+    if (this.back >= this.values.length - 1)
+      {
         this.back = 0;
-        return this.back;
-      }
-    else return this.back++;
-
+        return this.values.length - 1;
+      } // if
+    else
+      return this.back++;
   } // back()
-}
+} // class ImprovedArrayBasedQueue
